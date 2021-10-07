429bf5f948c0f9ebd9ce9c953609d259216862b2
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-10-07 17:51:17 +0300

    Add files via upload

---
+++ Șiruri de caractere ex6.py
@@ -0,0 +1,9 @@
+print("Introdu textul")
+x = str(input())
+x1 = x[0]
+x2 = x[-1]
+print(x1==x2)
+if(x1==x2):
+    print("Primul caracter este identic cu ultimul introdus ")
+else:
+        print("Primul caracter nu este identic cu ultimul caracter")
