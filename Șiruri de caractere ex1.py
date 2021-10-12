949c204a86e84be781bd34fc5bbd73c1d77f72ba
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-10-12 18:39:35 +0300

    Update Șiruri de caractere ex1.py

--- Șiruri de caractere ex1.py
+++ Șiruri de caractere ex1.py
@@ -1,5 +1,16 @@
+# Realizați un program care citește de la tastatură un șir de caractere și afișează de câte ori apare litera "a" în text.
+
+#Exemplu. Pentru "Mihai este la mare." se va afișa 3.
+#Rezolvare. Simplu – folosind metoda count():
+
+#s = input()
+#print(s.count("a"))
+#Exercițiu propus. Realizați un program similar care afișează de câte ori apare
+#fiecare vocală din alfabetul englezesc în textul citit și apoi la final, totalul
+#aparițiilor lor. Atenție la detalii – mai jos e stilizat puțin rezultatul:
 print("Textul dumneavoastra este:")
 a = str(input())
+# a este variabiala pentru text
 print("Vocale:")
 print(("a - "), a.count("a") )
 print(("e - "), a.count("e") )
